c601dee8de9a9a773dd16d50c5165f4b737534b8
KFischer(M13) <klaus.fischer@eloware.com>
2020-10-09 10:04:11 +0200

    Cleaning up file structure

--- ApiKeyFilter/Dto/ApiKeyDto.cs
+++ ApiKeyFilter/Dto/ApiKeyDto.cs
@@ -6,12 +6,8 @@ namespace ApiKeyFilter.Dto {
     public class ApiKeyDto {
         public string Id { get; set; }
         public DateTime Created { get; set; }
+        public DateTime? Deleted { get; set; }
         public string Description { get; set; }
         public IList<RoleDto> Roles { get; set; }
-    }
-
-    public class RoleDto  {
-        public string Id { get; set; }
-        public DateTime Created { get; set; }
     }
 }
